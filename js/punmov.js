71f4a0f629b427e3966355c989bef513c52574d2
SahiZahidi <101656096+SahiZahidi@users.noreply.github.com>
2022-06-16 18:54:15 +0530

    Create punmov.js

---
+++ js/punmov.js
@@ -0,0 +1,111 @@
+var PunMov=[
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_321,h_482,c_scale,f_webp,q_auto:eco/resources/0-0-1z5127658/portrait/1920x7705069b6f0976a43ada79dde78f78400a1.jpg",
+        moviename:"Main Viyah Nahi Karona Tere Naal Movie",
+        cat:"Romance",
+    },
+    
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_321,h_482,c_scale,f_webp,q_auto:eco/resources/0-0-1z5100806/portrait/1920x770ca82f8f4d86b40dfbca8819dd8aa8f9a.jpg" ,
+        moviename:"Yaar Anmulle Returns Movie",
+        cat:"Drama",
+    },
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_321,h_482,c_scale,f_webp,q_auto:eco/resources/0-0-1z591197/portrait/1920x77062a71c6b7f134f408d409cf9f2a49db2.jpg",
+        moviename:"Mera Vyah Kara Do Movie",
+        cat:"Romance",
+    },
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_321,h_482,c_scale,f_webp,q_auto:eco/resources/0-0-1z575318/portrait/1920x77002a5d3251d9c4fcc8982b7cc70238a05.jpg",
+        moviename:"Marjaney Movie",
+        cat:"Drama",
+    },
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_321,h_482,c_scale,f_webp,q_auto:eco/resources/0-0-1z555054/portrait/1920x77054ac35b3a0714182bdadcfbbe4988232.jpg",
+        moviename:"Fuffad Ji Movie",
+        cat:"Comedy",
+    },
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_321,h_482,c_scale,f_webp,q_auto:eco/resources/0-0-1z51682/portrait/1920x770cb3877585d064447969daa6e90827efd.jpg",
+        moviename:"Puaada Movie",
+        cat:"Comedy",
+    },
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_321,h_482,c_scale,f_webp,q_auto:eco/resources/0-0-1z520724/portrait/1920x77084f6817d734b47789394b1f5e9343dd1.jpg"  ,
+        moviename:"Jinne Jamme Saare Nikamme Movie",
+        cat:"Comedy",
+    },
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_321,h_482,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-0-74622/portrait/satyamevajayate298776966.jpg",
+        moviename:"Satyameva Jayate",
+        cat:"Action",
+    },
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-1z512446/portrait/1920x770dbc95b0b237a44fc815f8172f7178aa9.jpg" ,
+        moviename:"Tara Mira Movie",
+        cat:"Horror",
+    },
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-1z529263/portrait/1920x77053ca64a3a5a948919937b81eb7f2a7f8cb719fc89bb14adaaa3a92c09ff55aa0.jpg"  ,
+        moviename:"Qismat 2 Movie",
+        cat:"Romance",
+    },
+
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_321,h_482,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-0-399328/portrait/1920x77094fc5e4c855e4875bd8d022eb2c6ca5f.jpg",
+        moviename:"Radhe - Your Most Wanted Bhai",
+        cat:"Action",
+    },
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_321,h_482,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-0-35166/portrait/1920x770a6695f542cf3419291a6bc299aa894f3.jpg",
+        moviename:"Simba",
+        cat:"Action",
+    },
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_321,h_482,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-0-96839/portrait/1920x77062fa79ad2a00435ca0ef053f6a476a74.jpg",
+        moviename:"Dream Girl",
+        cat:"Romance",
+    },
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_321,h_482,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-0-27132/portrait/1920x770d1edc80582d845ffadbb89aa802c967b5ee1a2f6dd514d9bb193c3c269165cc3.jpg",
+        moviename:"Kedarnath",
+        cat:"Romance",
+    },
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_321,h_482,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-0-307715/portrait/00307715incov1533673836.jpg",
+        moviename:"Suraj Pe Mangal Vari",
+        cat:"Comedy",
+    },
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_321,h_482,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-0-33204/portrait/1920x770ab13e3472dce436a9e87bcd9b77bfad4.jpg",
+        moviename:"URI",
+        cat:"Action",
+    },
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_321,h_482,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-0-toiletekpremkatha/portrait/1920x7701497425846.jpg",
+        moviename:"Toilet",
+        cat:"Comedy",
+    },
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_321,h_482,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-0-6728/portrait/006728incover12314315123104ac0c30c9504cd28e37143696169d67.jpg",
+        moviename:"MULK",
+        cat:"Thriller",
+    },
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_321,h_482,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-0-122732/portrait/00122732incov1352570625135217a753ba57e64c269633ffe0a6fed413.jpg",
+        moviename:"Hotel Mumbai",
+        cat:"Thriller",
+    },
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-movie_1203346850/portrait/saabbahadar1920x73629616136d5621ce8d8a846078b01857b4ff60b54.jpg" ,
+        moviename:"Saab Bahadar Movie",
+        cat:"Thriller",
+    },
+
+  
+]
+
+localStorage.setItem("PunMov",JSON.stringify(PunMov));
+
+
+ 
